b79c694f12144ebe281de84f4fe4d6bbc94622c8
razvanbela <razvanvirgil.bela@ulbsibiu.ro>
2022-07-27 11:06:41 +0300

    Initial commit

---
+++ app/public/index.php
@@ -0,0 +1,43 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="main.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+</head>
+
+<body>
+<div class="container">
+    <div class="calendar">
+        <div class="month">
+            <i class="fas fa-angle-left prev"></i>
+            <div class="date">
+                <h1></h1>
+                <p></p>
+            </div>
+            <i class="fas fa-angle-right prev"></i>
+        </div>
+        <div class="weekdays">
+            <div>Sun</div>
+            <div>Mon</div>
+            <div>Tue</div>
+            <div>Wed</div>
+            <div>Thu</div>
+            <div>Fri</div>
+            <div>Sat</div>
+        </div>
+        <div class="days">
+        </div>
+    </div>
+</div>
+
+<script src="main.js"></script>
+
+</body>
+</html>
---
+++ app/public/main.js
@@ -0,0 +1,45 @@
+const date = new Date();
+
+const readerCalendar = () => {
+    date.setDate(1);
+    const monthDays = document.querySelector('.days');
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+    const firstDayIndex = date.getDay();
+    const lastDayIndex = date.getDay(date.getFullYear(), date.getMonth() + 1, 0);
+    const nextDays = 7 - lastDayIndex - 1;
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    document.querySelector(".date h1").innerHTML = months[date.getMonth()] + "   " + date.getFullYear();
+    document.querySelector(".date p").innerHTML = new Date().toDateString();
+    let days = "";
+    for (let i = firstDayIndex; i > 0; i--) {
+        days += '<div class="prev-date">${prevLastDay-i+1}</div>';
+    }
+    for (let j = 1; j <= lastDay; j++) {
+        if (j == new Date().getDate() && date.getMonth() === new Date().getMonth() && date.getFullYear() === new Date().getFullYear()) {
+            days += '<div class="today">${j}</div>>';
+        } else {
+            days += '<div class="days">${j}</div>';
+        }
+    }
+    for (let k = 1; k <= nextDays; k++) {
+        days += '<div class="next-date"></div>';
+        monthDays.innerHTML = days;
+    }
+};
+
+document.querySelector(".prev").addEventListener('click', () => {
+    date.setMonth(date.getMonth() - 1);
+    readerCalendar();
+});
+document.querySelector(".next").addEventListener('click', () => {
+    date.setMonth(date.getMonth() + 1);
+    readerCalendar();
+});
+readerCalendar();
+/*
+document.querySelectorAll('.days').forEach(item => {
+    item.addEventListener('click', event => {
+        alert(event.target.innerText)
+    })
+});*/
